bcfc1d328660f601e1922ef80ebc90d067ae8170
woshichichi <88015309+woshichichi@users.noreply.github.com>
2024-12-06 19:49:35 +0800

    23

--- src/App.tsx
+++ src/App.tsx
@@ -30,32 +30,12 @@ const App: React.FC = () => {
           }}>
           {videoState ? (
             <div style={{ width: '100%', height: '450px' }} onClick={() => { }}>
-              {descriptions.manufacturer === 'StarSoft' ? (
-                <MpegtsVideo url={videoSrc} />
-              ) : descriptions.manufacturer === 'GuangDa' ? (
-                <GuangPlayer
-                  // url={videoStates[item.uid].url}
-                  // userKey={cameraInfo?.userKey || '19ac88da-33bc-4ce9-a3a1-63ec2bc8d2ff'}
-                  // 返回"url": "wss://plugvideo.vocsystem.cn:4443_19ac88da-33bc-4ce9-a3a1-63ec2bc8d2ff",取_后面部分
-                  userKey={cameraInfo?.url?.split('_')[1]}
-                  baseURL={cameraInfo?.url?.split('_')[0]}
-                  userId={cameraInfo?.userId || 'apitest'}
-                  device={cameraInfo?.uid?.split('_')[0]}
-                  channel={cameraInfo?.uid?.split('_')[1]}
-                  protocolType={cameraInfo?.protocolType || 1}
-                  SpecialSign={cameraInfo?.SpecialSign || 0}
-                  plate={cameraInfo?.name}
-                  vehicleId={cameraInfo?.vehicleId || '4321'}
-                  groupId={cameraInfo?.groupId || '2'}
-                />
-              ) : (
-                <HikH5Player
-                  options={{
-                    url: videoSrc,
-                    border: '#fff',
-                  }}
-                />
-              )}
+              <HikH5Player
+                options={{
+                  url: videoSrc,
+                  border: '#fff',
+                }}
+              />
             </div>
           ) : (
             <>
@@ -103,7 +83,7 @@ const App: React.FC = () => {
                   textAlign: 'left',
                   padding: '0px 10px',
                 }}>
-                {descriptions?.name}
+                {'askdlfjsl'}
               </span>
             </>
           )}
@@ -128,7 +108,6 @@ const App: React.FC = () => {
           )}
         </div>
       </ul>
-      />
 
     </div>
   );
